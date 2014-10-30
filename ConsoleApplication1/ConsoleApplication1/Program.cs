49e4b3ca25c645fba3023810855038df9476ac7c
chasehansen10 <chase.hansen10@gmail.com>
2014-10-29 20:48:45 -0600

    FlipMania

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 30; i++)
+            {
+                if (i % 3 == 0&&i%5==0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (i % 3==0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            LetterFinder("f", "ffffuck");
+            
+            Console.ReadKey();
+        }
+        static void LetterFinder(string letter, string word)
+        {
+            int count = 0;
+            
+            
+            for(int i = 0; i<word.Length;i++)
+            {
+                string currentLetter = word[i].ToString().ToLower();
+                if(letter.ToLower()== currentLetter)
+                {
+
+                    count++;
+                }
+
+            }
+            Console.WriteLine(word+" has "+count+" "+letter+"'s in it");
+        }
+        static void WordFinder(string word, string bunchOfWords)
+        {
+            int wordCounter = 0;
+            
+
+        }
+
+    }
+}
---
+++ FlipMania/FlipMania/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipMania
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FlipCoins(10000);
+            FlipForHeads(10000);
+            Console.ReadKey();
+        }
+        //creates new function
+        static void FlipCoins(int numberOfFlips)
+        {
+            //setup for the loop and counters
+            int numberOfHeads = 0;
+            int numberOfTails = 0;
+            Random rng = new Random();
+            
+            for(int i=0; i<numberOfFlips; i++)
+            {
+                int randomNumber = rng.Next(0, 2);
+                if(randomNumber==0)
+                {
+                    numberOfHeads++;
+                }
+                else
+                {
+                    numberOfTails++;
+                }
+            }
+            Console.WriteLine("We flipped a coin " + numberOfFlips + " times.");
+            Console.WriteLine("Number of heads: " + numberOfHeads);
+            Console.WriteLine("Number of tails: " + numberOfTails);
+        }
+        static void FlipForHeads(int numberOfHeads)
+        {
+            //setup for my numberOfHeadsFlipped and totalFlips
+            int numberOfHeadsFlipped = 0;
+            int totalFlips = 0;
+            Random rng= new Random();
+            //loops until numheadflip
+            while(numberOfHeadsFlipped<=numberOfHeads)
+            {
+                totalFlips++;
+                int randomNumber=rng.Next(0, 2);
+                if(randomNumber==0)
+                {
+                    numberOfHeadsFlipped++;
+                }
+
+                
+            }
+            
+            Console.WriteLine("We are flipping a coin until we find " + numberOfHeads + " heads.");
+            Console.WriteLine("It took " + totalFlips + " to find " + numberOfHeads + " heads.");
+            
+            
+
+            
+
+        }
+    }
+}
